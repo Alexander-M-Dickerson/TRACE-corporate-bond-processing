41368f5c221785be14c8c1ad0777f9ef82b59db8
Alex Dickerson <56037913+Alexander-M-Dickerson@users.noreply.github.com>
2023-08-03 22:04:08 +0100

    Update MakeBondIntra_Daily.py

--- TRACE/MakeBondIntra_Daily.py
+++ TRACE/MakeBondIntra_Daily.py
@@ -772,6 +772,6 @@ VolumeExport = pd.concat(volume_super_list, axis=0     , ignore_index=False)
 
 # Save in compressed GZIP format # 
 PricesExport.to_csv('Prices_BBW_TRACE_Enhanced_Dick_Nielsen.csv.gzip'     , compression='gzip')   
-VolumesExport.to_csv('Volumes_BBW_TRACE_Enhanced_Dick_Nielsen.csv.gzip'   , compression='gzip')     
+VolumeExport.to_csv('Volumes_BBW_TRACE_Enhanced_Dick_Nielsen.csv.gzip'   , compression='gzip')     
 CleaningExport.to_csv('Cleaning_TRACE_Enhanced_Dick_Nielsen.csv') 
 # =============================================================================     
