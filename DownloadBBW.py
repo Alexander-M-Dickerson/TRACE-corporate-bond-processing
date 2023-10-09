cdf53643e0254ed87e54d1254fef3f09b4fc096e
Alex Dickerson <56037913+Alexander-M-Dickerson@users.noreply.github.com>
2023-10-09 15:17:50 +1100

    Update DownloadBBW.py

--- DownloadBBW.py
+++ DownloadBBW.py
@@ -4,7 +4,7 @@ import numpy as np
 import urllib.request
 
 # Load data      #
-_url = 'https://openbondassetpricing.com/wp-content/uploads/2023/09/bbw_wrds_oct_2023_lastest.csv'
+_url = 'https://openbondassetpricing.com/wp-content/uploads/2023/10/bbw_wrds_oct_2023_lastest.csv'
 all_factors = pd.read_csv(_url)
 
 # End #
